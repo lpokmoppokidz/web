e9666139b452b955f32fe5570bbe40b2137126a7
por-emma <121848247+por-emma@users.noreply.github.com>
2023-05-04 18:13:32 +0700

    update trang for-her

---
+++ js/header.js
@@ -0,0 +1,24 @@
+const open = document.getElementById("search");
+const close = document.getElementById("close");
+const container = document.getElementById("container");
+open.addEventListener("click", () => {
+    container.classList.add("active")
+})
+close.addEventListener("click", () => {
+    container.classList.remove("active")
+})
+
+const hambuger = document.getElementById("menu");
+const close1 = document.getElementById("close1");
+const container1 = document.getElementById("container1");
+hambuger.addEventListener("click", () => {
+    container1.classList.add("active")
+})
+close1.addEventListener("click", () => {
+    container1.classList.remove("active")
+})
+const container2 = document.getElementById("container2");
+const close2 = document.getElementById("close2");
+close2.addEventListener("click", () => {
+    container2.classList.remove("active");
+});
